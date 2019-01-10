9576dc8aef6e021ff3616de908611a8b14ed5902
Mousa Ayoubi <mousaayoubi@gmail.com>
2019-01-10 16:51:09 +0200

    Update trackster.js

--- public/resources/js/trackster.js
+++ public/resources/js/trackster.js
@@ -17,28 +17,12 @@ var API_KEY = "25b460cefc5e2f646c570f993ac29d97";
 
 var Trackster = {};
 
-/*
-  Given an array of track data, create the HTML for a Bootstrap row for each.
-  Append each "row" to the container in the body to display all tracks. 
-*/
-Trackster.renderTracks = function(tracks) {
-  
-};
-
-/*
-  Given a search term as a string, query the LastFM API.
-  Render the tracks given in the API query response.
-*/
 Trackster.searchTracksByTitle = function(title) {
   $.ajax({
     url: "https://ws.audioscrobbler.com/2.0/?method=track.search&track="+title+"&api_key="+API_KEY+"&format=json",
     success: function(result) {
-      console.log(result);
-      Trackster.renderTracks();
       var result_obj = result;
       var n = result_obj.results.trackmatches.track.length;
-      console.log(n);
-      $(".logo").animate({height: "toggle"});
       for (var i = 0; i < n; i++) {
         var album = result_obj.results.trackmatches.track[i].image[1]["#text"];
         var formattedHtml = 
@@ -64,7 +48,6 @@ Trackster.searchTracksByTitle = function(title) {
         '</a>';            
         $("#results").append(formattedHtml);
       }
-      $(".logo").animate({height: "toggle"});
     }
   });
 };
