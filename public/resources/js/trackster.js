32d2340106bc21f4e1f8b536cfb88ccf3b399608
Mousa Ayoubi <mousaayoubi@gmail.com>
2019-01-14 19:38:25 +0200

    Update to load success.hmtl on same page upon button click

--- public/resources/js/trackster.js
+++ public/resources/js/trackster.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
   $(".form-control").keydown(function(e) {
     if (e.which == 13) {
       localStorage.setItem("byTitle", $(".form-control").val());
-      window.open("./success.html");
+      window.open("./success.html", "_self");
       return false;
     }
   })
 
   $(".btn").click(function() {
     localStorage.setItem("byTitle", $(".form-control").val());
-    window.open("./success.html");
+    window.open("./success.html", "_self");
   });
 });
